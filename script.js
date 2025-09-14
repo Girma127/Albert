578db62e3909cf6678e5bb7cf9c576d618d00b41
Girma127 <girmamoloro326@gmail.com>
2025-09-15 02:38:26 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const swiper=new Swiper('.swiper',{
+  loop:true,
+  slidesPerView:1,
+  spaceBetween:20,
+  autoplay:{
+    delay:3000,
+    disableOnInteraction:false,
+  },
+  pagination:{
+    el:'.swiper-pagination',
+    clickable:true
+  },
+  breakpoints: {
+    640: { slidesPerView: 3 }
+  }
+})
